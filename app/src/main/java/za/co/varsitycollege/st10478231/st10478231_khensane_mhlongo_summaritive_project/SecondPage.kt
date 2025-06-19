61df8d37836877838cdca11317f6d50c0cfce585
ST10478231 <ST10478231@VCConnect.edu.za>
2025-06-19 10:53:09 +0200

    Used a for loop Created two new buttons Got information from the first app

--- app/src/main/java/za/co/varsitycollege/st10478231/st10478231_khensane_mhlongo_summaritive_project/SecondPage.kt
+++ app/src/main/java/za/co/varsitycollege/st10478231/st10478231_khensane_mhlongo_summaritive_project/SecondPage.kt
@@ -1,5 +1,6 @@
 package za.co.varsitycollege.st10478231.st10478231_khensane_mhlongo_summaritive_project
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -19,12 +20,36 @@ class SecondPage : AppCompatActivity() {
             insets
         }
         val txeInputSongTitle = intent.getStringArrayExtra("txeInputSongTitle")
-        val txeInputArtists =
+        val txeInputArtists = intent.getStringArrayExtra("txeInputArtists")
+        val txeInputRatings = intent.getStringArrayExtra("txeInputRatings")
+        val txeInputComments = intent.getStringArrayExtra("txeInputComments")
         val btnView = findViewById<Button>(R.id.btnView)
         val txeShowcasePlaylist = findViewById<TextView>(R.id.txeShowcasePlaylist)
         val btnBack = findViewById<Button>(R.id.btnBack)
         val btnExit2 = findViewById<Button>(R.id.btnExit2)
 
 
+        btnView.setOnClickListener {
+            if (txeInputSongTitle != null && txeInputArtists != null && txeInputRatings != null && txeInputComments != null) {
+                val builder = StringBuilder()
+                val count = txeInputSongTitle.size
+                for (i in 0 until count) {
+                    builder.append("Song Title: ${txeInputSongTitle[i]}\n")
+                    builder.append("Artist: ${txeInputSongTitle.getOrNull(i) ?: ""}\n")
+                    builder.append("Rating : ${txeInputSongTitle.getOrNull(i) ?: ""}\n")
+                    builder.append("Comment: ${txeInputSongTitle.getOrNull(i) ?: ""}\n\n")
+                }
+                txeShowcasePlaylist.text = builder.toString()
+            }else {
+                txeShowcasePlaylist.text = "There was no information provided."
+            }
+            btnBack.setOnClickListener {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            btnExit2.setOnClickListener {
+                finishAffinity()
+            }
+        }
     }
 }
